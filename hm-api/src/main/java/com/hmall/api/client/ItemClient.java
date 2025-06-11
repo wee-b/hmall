ee49502f0731c1b50df9ab9fc017676e2c2b4449
wee <19064060958@163.com>
2025-06-11 23:46:30 +0800

    为每个微服务添加Sentinel(用于分布式系统的流量控制、熔断降级、系统负载保护),同时采用seata来确保数据一致性

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -1,15 +1,20 @@
 package com.hmall.api.client;
 
 
+import com.hmall.api.client.fallback.ItemClientFallback;
+import com.hmall.api.config.DefaultFeignConfig;
 import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
 import java.util.Collection;
 import java.util.List;
 
-@FeignClient("item-service")
+@FeignClient(value = "item-service",
+        configuration = DefaultFeignConfig.class,
+        fallbackFactory = ItemClientFallback.class)
 public interface ItemClient {
 
     @GetMapping("/items")
